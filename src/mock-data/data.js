2c0678afcea434357a2f286bd9a3cd36e5c7b9a7
Jovan Babic <jbabic999@gmail.com>
2022-03-22 20:07:30 +0100

    Changed the data array to be filled with more random data

--- src/mock-data/data.js
+++ src/mock-data/data.js
@@ -1,44 +1,12 @@
 import { faker } from '@faker-js/faker';
 
-export const data = [
-    {
-        id: 1,
-        name: 'Jovan',
-        surname: 'Babic',
-        jmbg: '1234567890123',
-        email: 'jbabic3018rn@raf.rs',
-        position: 'Junior Software Engineer',
-        phone: '+381601234567',
-        active: true
-    },
-    {
-        id: 2,
-        name: faker.name.firstName(),
-        surname: faker.name.lastName(),
-        jmbg: faker.datatype.number({ min: 1000000000000, max: 9999999999999 }).toString(),
-        email: faker.internet.email(),
-        position: faker.name.jobTitle(),
-        phone: faker.phone.phoneNumber(),
-        active: true
-    },
-    {
-        id: 3,
-        name: faker.name.firstName(),
-        surname: faker.name.lastName(),
-        jmbg: faker.datatype.number({ min: 1000000000000, max: 9999999999999 }).toString(),
-        email: faker.internet.email(),
-        position: faker.name.jobTitle(),
-        phone: faker.phone.phoneNumber(),
-        active: true
-    },
-    {
-        id: 4,
-        name: faker.name.firstName(),
-        surname: faker.name.lastName(),
-        jmbg: faker.datatype.number({ min: 1000000000000, max: 9999999999999 }).toString(),
-        email: faker.internet.email(),
-        position: faker.name.jobTitle(),
-        phone: faker.phone.phoneNumber(),
-        active: true
-    }
-]
+export const data = Array.from({ length: 20 }, (_, index) => ({
+    id: index + 1,
+    name: faker.name.findName(),
+    surname: faker.name.lastName(),
+    jmbg: faker.random.number({ min: 100000000, max: 999999999 }),
+    email: faker.internet.email(),
+    position: faker.name.jobTitle(),
+    phone: faker.phone.phoneNumber(),
+    active: faker.random.boolean(),
+}));
