4bb302791b645f06bd9f0b68b0ff5c8197fe3033
Jovan Babic <jovanbabic@Jovans-MacBook-Pro.local>
2022-03-23 15:32:49 +0100

    Added footer and made navbar stick on top

---
+++ src/components/Footer.vue
@@ -0,0 +1,9 @@
+<template>
+    <footer class="footer mt-auto py-3 bg-dark fixed-bottom">
+        <div class="container">
+            <span class="text-muted text-center">
+                © 2022 Copyright: Racunarski Fakultet
+            </span>
+        </div>
+    </footer>
+</template>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
         <div class="container">
             <a class="navbar-brand" href="">RAF Insurance</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
