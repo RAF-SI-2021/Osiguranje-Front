22f3241c6e18d2fc8840d8c5b8af10cdcea19e34
Jovan Babic <jovanbabic@Jovans-MacBook-Pro.local>
2022-03-23 14:23:07 +0100

    Added Admin page with navigation links

---
+++ src/views/AdminView.vue
@@ -0,0 +1,34 @@
+<script setup></script>
+
+<template>
+  <div>
+    <h1 class="text-center mt-5">Welcome, Admin</h1>
+    <div class="container">
+      <h3 class="mt-5">Navigate to:</h3>
+      <div class="row">
+        <div class="col-4 p-3">
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">
+                <RouterLink to="/admin/users">All Users</RouterLink>
+              </h5>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-4 p-3">
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">
+                <RouterLink to="#">Add New User</RouterLink>
+              </h5>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style></style>
