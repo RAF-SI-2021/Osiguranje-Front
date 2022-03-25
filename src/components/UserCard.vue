a618b34cfd63bd277e2c5c96eda2425bd3e697d4
Bogdan Stojadinovic <56963866+bstojadinovic17@users.noreply.github.com>
2022-03-25 14:51:16 +0100

    Update UserCard component
    Container div removed

--- src/components/UserCard.vue
+++ src/components/UserCard.vue
@@ -1,22 +1,20 @@
 <template>
-    <div class="container">
-        <div class="card p-4" style="width: 18rem;" >
-            <div class="d-flex justify-content-between">
-                <div>
-                    <h5 class="card-title">Name Surname<!--{{name}}, {{surname}} --></h5>
-                </div>
-                <div>
-                    <button type="button" class="btn btn-primary btn-sm text-end" @click="editUser({id})"> Edit user</button>
-                </div>
-            </div>
-            <hr>
+    <div class="card p-4" style="width: 18rem;" >
+        <div class="d-flex justify-content-between">
             <div>
-                <p>JMBG: {{ jmbg }} </p>
-                <p>Email: {{ email }}</p>
-                <p>Phone: {{ phone }}</p>
-                <p>Position: {{ position }}</p>
-                <p>Active: {{ active }}</p>
+                <h5 class="card-title">Name Surname<!--{{name}}, {{surname}} --></h5>
             </div>
+            <div>
+                <button type="button" class="btn btn-primary btn-sm text-end" @click="editUser({id})"> Edit user</button>
+            </div>
+        </div>
+        <hr>
+        <div>
+            <p>JMBG: {{ jmbg }} </p>
+            <p>Email: {{ email }}</p>
+            <p>Phone: {{ phone }}</p>
+            <p>Position: {{ position }}</p>
+            <p>Active: {{ active }}</p>
         </div>
     </div>
 </template>
