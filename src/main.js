5fd5a8c7595df9bb755d3bf01171b2b68d8f5a71
Bogdan Stojadinovic <56963866+bstojadinovic17@users.noreply.github.com>
2022-04-02 18:11:10 +0200

    Update main.js
    Minor updates in main.js regarding importing pinia and simple typeahead

--- src/main.js
+++ src/main.js
@@ -1,12 +1,17 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import SimpleTypeahead from 'vue3-simple-typeahead';
+import {createPinia} from "pinia";
 
 const app = createApp(App)
 
 app.use(router)
+app.use(SimpleTypeahead);
+app.use(createPinia());
 
 app.mount('#app')
 
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap"
+import 'vue3-simple-typeahead/dist/vue3-simple-typeahead.css';
