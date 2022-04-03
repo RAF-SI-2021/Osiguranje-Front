007feb9af9a6ae6e9bcf9fa32d4573c69518f06a
Slavko Kofileski <101840117+skofileski@users.noreply.github.com>
2022-04-03 18:09:47 +0200

    Small changes

--- src/views/StockMarketListView.vue
+++ src/views/StockMarketListView.vue
@@ -7,36 +7,54 @@ export default {
     console.log(stocks);
 
     const obj = reactive({
-      market: stocks
+      market: stocks,
     });
-    return {obj}
+    return { obj };
   },
 };
 </script>
 
 <template>
   <div class="align-middle">
-    <table class="table">
-      <thead>
-        <tr>
-          <th scope="col">#</th>
-          <th scope="col">Stock Name</th>
-          <th scope="col">Stock Symbol</th>
-          <th scope="col">Country</th>
-          <th scope="col">Currency</th>
-          <th scope="col">Time Zone</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="stock in obj.market" :key="stock.id">
-          <th scope="row"></th>
-          <td>{{stock.stock_name}}</td>
-          <td>{{stock.symbol}}</td>
-          <td>{{stock.country}}</td>
-          <td>{{stock.currency}}</td>
-          <td>{{stock.time_zone}}</td>
-        </tr>
-      </tbody>
-    </table>
+    <div class="col-12 col-md-9 col-lg-7 col-xl-6">
+      <table class="table table-bordered" >
+        <thead>
+          <tr bgcolor = "#80aaff">
+            
+            <th scope="col">Stock Name</th>
+            <th scope="col">Stock Symbol</th>
+            <th scope="col">Country</th>
+            <th scope="col">Currency</th>
+            <th scope="col">Time Zone</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="stock in obj.market" :key="stock.id">
+            
+            <td>{{ stock.stock_name }}</td>
+            <td>{{ stock.symbol }}</td>
+            <td>{{ stock.country }}</td>
+            <td>{{ stock.currency }}</td>
+            <td>{{ stock.time_zone }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
+
+<style scoped>
+
+table {
+  counter-reset: rowNumber;
+}
+
+table tr::before {
+  display: table-cell;
+  counter-increment: rowNumber;
+  content: counter(rowNumber) ".";
+  padding-right: 0.3em;
+  text-align: left;
+}
+    
+</style>
