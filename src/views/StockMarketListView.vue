47c29231ca245a9e963d0c648166600b5595f12d
Slavko Kofileski <101840117+skofileski@users.noreply.github.com>
2022-04-03 18:30:22 +0200

    Small changes

--- src/views/StockMarketListView.vue
+++ src/views/StockMarketListView.vue
@@ -16,29 +16,31 @@ export default {
 
 <template>
   <div class="align-middle">
-    <div class="col-12 col-md-9 col-lg-7 col-xl-6">
-      <table class="table table-bordered" >
-        <thead>
-          <tr bgcolor = "#80aaff">
-            
-            <th scope="col">Stock Name</th>
-            <th scope="col">Stock Symbol</th>
-            <th scope="col">Country</th>
-            <th scope="col">Currency</th>
-            <th scope="col">Time Zone</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="stock in obj.market" :key="stock.id">
-            
-            <td>{{ stock.stock_name }}</td>
-            <td>{{ stock.symbol }}</td>
-            <td>{{ stock.country }}</td>
-            <td>{{ stock.currency }}</td>
-            <td>{{ stock.time_zone }}</td>
-          </tr>
-        </tbody>
-      </table>
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-12 col-md-9 col-lg-7 col-xl-6">
+          <table class="table table-bordered">
+            <thead>
+              <tr bgcolor="#80aaff">
+                <th scope="col">Stock Name</th>
+                <th scope="col">Stock Symbol</th>
+                <th scope="col">Country</th>
+                <th scope="col">Currency</th>
+                <th scope="col">Time Zone</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="stock in obj.market" :key="stock.id">
+                <td>{{ stock.stock_name }}</td>
+                <td>{{ stock.symbol }}</td>
+                <td>{{ stock.country }}</td>
+                <td>{{ stock.currency }}</td>
+                <td>{{ stock.time_zone }}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   </div>
 </template>
