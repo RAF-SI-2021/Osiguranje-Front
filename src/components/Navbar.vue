e37025df1036e5b30f517d8becf529d235a52259
Jovan Babic <jbabic999@gmail.com>
2022-04-04 19:49:36 +0200

    Styled edit user and added validation

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -3,7 +3,7 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container">
-      <a class="navbar-brand" href="">RAF Insurance</a>
+      <RouterLink to="/" class="navbar-brand">RAF Insurance</RouterLink>
       <button
         class="navbar-toggler"
         type="button"
@@ -18,13 +18,7 @@
       <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
         <ul class="navbar-nav ms-auto">
           <li class="nav-item">
-            <a class="nav-link" href="">Contact</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="">Pricing</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="">Download</a>
+            <RouterLink to="/login" class="nav-link">Log In</RouterLink>
           </li>
         </ul>
       </div>
--- src/views/user/EditUser.vue
+++ src/views/user/EditUser.vue
@@ -1,28 +1,72 @@
 <template>
     <div class="container user-edit-container">
-        <h1 class="mt-3 text-center">User: {{state.param_userid}} </h1>
-        <div class="row justify-content-center"></div>
+      <h1 class="mt-3 mb-3 text-center">Editing User</h1>
+      <div class="row justify-content-center"></div>
 
-        <!-- TODO: Replace the div within the row with the UserCard component -->
-        <div class="row" v-if="state.current_user">
-            <div class="col-6 p-3">
-                <p>Name: <input v-model="state.current_user.name"> </p>
-                <p>Surname: <input v-model="state.current_user.surname"></p>
-                <p>JMBG: <input v-model="state.current_user.jmbg"></p>
-                <p>Email: <input v-model="state.current_user.email"></p>
-                <p>Phone: <input v-model="state.current_user.phone"></p>
-                <p>Position: <input v-model="state.current_user.position"></p>
-                <p>Active: <ToggleButton v-model="state.current_user.active" @input="valueChanged"></ToggleButton> {{state.current_user.active}}</p>
+      <div class="row justify-content-center" v-if="user">
+        <form class="col-6" @submit="submit">
+          <div class="mb-3">
+            <label class="form-label">Name:</label>
+            <input type="text" class="form-control" v-model="user.name">
+            <div v-for="error in v$.name.$errors" :key="error.$uid">
+              <span>{{error.$message}}</span>
             </div>
-        </div>
+          </div>
+          <div class="mb-3">
+            <label class="form-label">Surname:</label>
+            <input type="text" class="form-control" v-model="user.surname">
+            <div v-for="error in v$.surname.$errors" :key="error.$uid">
+              <span>{{error.$message}}</span>
+            </div>
+          </div>
+          <div class="mb-3">
+            <label class="form-label">JMBG:</label>
+            <input type="number" class="form-control" v-model="user.jmbg">
+            <div v-for="error in v$.jmbg.$errors" :key="error.$uid">
+              <span>{{error.$message}}</span>
+            </div>
+          </div>
+          <div class="mb-3">
+            <label class="form-label">E-mail:</label>
+            <input type="email" class="form-control" v-model="user.email">
+            <div v-for="error in v$.email.$errors" :key="error.$uid">
+              <span>{{error.$message}}</span>
+            </div>
+          </div>
+          <div class="mb-3">
+            <label class="form-label">Phone Number:</label>
+            <input type="text" class="form-control" v-model="user.phone">
+<!--            <div v-for="error in v$.phone.$errors" :key="error.$uid">-->
+<!--              <span>{{error.$message}}</span>-->
+<!--            </div>-->
+          </div>
+          <div class="mb-3">
+            <label class="form-label">Position:</label>
+            <input type="text" class="form-control" v-model="user.position">
+            <div v-for="error in v$.position.$errors" :key="error.$uid">
+              <span>{{error.$message}}</span>
+            </div>
+          </div>
+          <div class="mb-3">
+            <label class="form-label">Active:</label>
+            <br>
+            <ToggleButton v-model="user.active" @input="valueChanged"></ToggleButton>
+          </div>
+          <div class="mb-3">
+            <button type="submit" class="btn btn-lg btn-primary">Confirm Edit</button>
+          </div>
+        </form>
+      </div>
     </div>
 </template>
 
 <script>
 import { data } from '../../mock-data/data';
-import {reactive, computed, onMounted } from 'vue';
+import {reactive, computed, onMounted, ref} from 'vue';
 import { useRoute } from 'vue-router'
 import ToggleButton from '../misc/ToggleButton.vue';
+import {email, maxLength, minLength, numeric, required} from "@vuelidate/validators";
+import useVuelidate from "@vuelidate/core";
 
 export default {
     displayName: "User Edit",
@@ -39,22 +83,55 @@ export default {
             return null;
         }
 
+      const rules = computed(() => {
+        return {
+          email:{required, minLength:minLength(4), maxLength:maxLength(24), email},
+          name:{required, minLength:minLength(2), maxLength:maxLength(24)},
+          surname:{required, minLength:minLength(2), maxLength:maxLength(24)},
+          jmbg:{required, minLength:minLength(13), maxLength:maxLength(13)},
+          position:{required},
+          phoneNumber:{required, numeric},
+        }
+      })
+
+      const state = reactive({
+        param_userid: "",
+        current_user: null
+      });
+
+      const user = ref({
+        name: '',
+        surname: '',
+        email: '',
+        jmbg: 0,
+        position: '',
+        phoneNumber: 0
+      })
+
+      const v$ = useVuelidate(rules, user);
+
+      onMounted(() => {
+        console.log(state);
+        state.param_userid = Number(route.params.id);
+        let userFetched = fetchUserData(state.param_userid);
+        console.log("user", userFetched);
+        state.current_user = userFetched;
+        user.value = userFetched;
+      });
+
         function valueChanged(val) {
             console.log("VALUE CHANGED", val);
         }
 
-        const state = reactive({
-            param_userid: "",
-            current_user: null
-        });
-        onMounted(() => {
-            console.log(state);
-            state.param_userid = Number(route.params.id);
-            let user = fetchUserData(state.param_userid);
-            console.log("user", user);
-            state.current_user = user;
-        });
-        return { state, valueChanged };
+
+      function submit(e) {
+        e.preventDefault();
+        this.v$.$validate();
+        console.log(this.v$)
+      }
+
+
+        return { state, valueChanged, submit, v$, user };
     },
     components: { ToggleButton }
 }
@@ -63,5 +140,9 @@ export default {
 <style scoped>
 .user-edit-container {
     margin-top:50px;
+}
+
+span {
+  color: red;
 }
 </style>
