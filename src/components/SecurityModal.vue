21a676ddf37793aa8349907f723c465416f91fc6
Petar Vujovic <pvujovic10818rn@raf.rs>
2022-05-30 23:33:58 +0200

    But/Sell modal with confirmation

--- src/components/SecurityModal.vue
+++ src/components/SecurityModal.vue
@@ -8,8 +8,19 @@ const props = defineProps({
     security: {
         type: Object,
         default: {}
+    },
+    myInput:{
+        type: Object,
+        default:{
+            brojHartija: 0,
+            limitValue: 0,
+            stopValue: 0,
+            allOrNone: false,
+            margin:false
+        }
     }
 })
+
 
 </script>
 
@@ -19,10 +30,39 @@ const props = defineProps({
             <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="modal-title">{{ props.action }}: {{ props.security.symbol }}</h5>
+                    <h1>{{props.myInput.brojHartija}}</h1>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
-                    <p>Object: {{ props.security }}</p>
+                    <div>
+                        <label for="broj_hartija">Unesite broj hartija: </label>
+                        <input v-model="props.myInput.brojHartija" type="number" class="form-control" id="broj_hartija" min="0">
+                    </div>
+
+                    <div>
+                        <label for="limit_value">Unesite limit value: </label>
+                        <input v-model="props.myInput.limitValue" type="number" class="form-control" id="limit_value" min="0">
+                    </div>
+
+                    <div>
+                        <label for="stop_value">Unesite stop value: </label>
+                        <input v-model="props.myInput.stopValue" type="number" class="form-control" id="stop_value" min="0">
+                    </div>
+                    
+                    <div>
+                        <label for="all_or_none_cb">
+                            All or none:
+                            <input v-model="props.myInput.allOrNone" type="checkbox" id="all_or_none_cb"> 
+                        </label>
+                    </div>
+
+                    <div>
+                        <label for="margin_cb">
+                            Margin: 
+                            <input v-model="props.myInput.margin" type="checkbox" id="margin_cb"> 
+                        </label>
+                    </div>
+                    
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
