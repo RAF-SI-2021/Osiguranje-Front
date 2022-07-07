47f32d65de5fac18a232db9be890e266df2ddfbf
Jovan Babic <jbabic999@gmail.com>
2022-07-07 16:01:15 +0200

    feat(otc): add company bank account

---
+++ src/api/companyAPI.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+export const companyAPI = {
+  getCompanies: () => {
+    return axios.get("/api/otc/companies", {
+      headers: {
+        "Authorization": `Bearer ${localStorage.getItem("token")}`
+      }
+    })
+  },
+
+  addCompanyBankAccount: (companyId, bankAccount) => {
+    return axios.post(`/api/otc/companies/${companyId}/bank-accounts`, bankAccount, {
+      headers: {
+        "Authorization": `Bearer ${localStorage.getItem("token")}`
+      }
+    })
+  }
+}
